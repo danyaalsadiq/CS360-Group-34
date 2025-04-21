072b3fc7ba4aa579562f16b299059e20246cdd58
muizz-1 <26100368@lums.edu.pk>
2025-04-21 19:37:47 +0500

    ...

--- server/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './slot';
-export * from './therapist-submission';
-export * from './student-request';
-export * from './cancellation-request';
--- server/services/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './slot-service';
-export * from './therapist-submission-service';
-export * from './student-request-service';
-export * from './cancellation-service';
-export * from './scheduler-service';
