fe3134a79a0e721a496b0a0f6da2919f74492e07
aleeshayirfan <aleeshayirfan@gmail.com>
2025-04-26 01:42:15 +0500

    Bug Fixes Aleeshay

--- server/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './slot-controller';
-export * from './therapist-submission-controller';
-export * from './student-request-controller';
-export * from './cancellation-controller';
-export * from './scheduler-controller';
